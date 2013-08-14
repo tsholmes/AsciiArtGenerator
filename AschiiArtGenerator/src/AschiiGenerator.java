c7d235d35d30540ec3a5db7c26df746a87d99d87
MadDogMcNamara <rjmcnama@mtu.edu>
2013-08-14 09:51:47 -0400

    Added save to file func

--- AschiiArtGenerator/src/AschiiGenerator.java
+++ AschiiArtGenerator/src/AschiiGenerator.java
@@ -15,6 +15,8 @@ import javax.swing.JOptionPane;
 
 public class AschiiGenerator {
 	
+	enum PictureDestination {Display, File, None};
+	
 	public static double sHeightToWidthRatio  ;
 	
 	PixelAnalyzer mPixelAnalyzer;
@@ -41,7 +43,7 @@ public class AschiiGenerator {
 	}
 
 
-	public String generateTextFromPicture( String aPicturePath, int aCols )
+	public String generateTextFromPicture( String aPicturePath, int aCols, PictureDestination aPictureDestination )
 	{
 		BufferedImage im = null;
 		try 
@@ -103,22 +105,63 @@ public class AschiiGenerator {
 			ret.append( System.lineSeparator() );		
 		}
 		
-		System.out.println(mPixelAnalyzer.getBuckets());
+		switch( aPictureDestination )
+		{
+		case Display:
+			// display result
+			BufferedImage lResultImage = new BufferedImage((int) (3000), (int)( 3000), BufferedImage.TYPE_INT_RGB);
+			Graphics lResultGraphics = lResultImage.createGraphics();
+			
+			lResultGraphics.setColor(Color.white);
+			lResultGraphics.fillRect(0, 0, lResultImage.getWidth(), lResultImage.getHeight());
+			
+			lResultGraphics.setFont( mFont );
+			lResultGraphics.setColor(Color.black);
+			drawString(lResultGraphics, ret.toString(), 0, 0);
+			
+			//lResultGraphics.drawString(ret.toString(), lResultImage.getWidth() / 2, lResultImage.getHeight() / 2);
+	        JLabel label = new JLabel(new ImageIcon(lResultImage));  
+	        JOptionPane.showMessageDialog(null, label);
+	        
+			break;
+		case File:
+			BufferedImage lResultImage1 = new BufferedImage((int) (3000), (int)( 3000), BufferedImage.TYPE_INT_RGB);
+			Graphics lResultGraphics1 = lResultImage1.createGraphics();
+			
+	        // store that font width and height
+	        FontMetrics bigFontMetrics = lResultGraphics1.getFontMetrics(mFont);
+
+			double width = bigFontMetrics.stringWidth("oooo") / 4 * lCols;
+			double height = bigFontMetrics.getHeight() * lRows;
+	        
+	        
+	        
+	        
+		    // display result
+			BufferedImage lSaveResultImage = new BufferedImage((int)width, (int)height, BufferedImage.TYPE_INT_RGB);
+			Graphics lSaveResultGraphics = lSaveResultImage.createGraphics();
+		 	
+			lSaveResultGraphics.setColor(Color.white);
+			lSaveResultGraphics.fillRect(0, 0, lSaveResultImage.getWidth(), lSaveResultImage.getHeight());
+			
+			lSaveResultGraphics.setFont( mFont );
+			lSaveResultGraphics.setColor(Color.black);
+			drawString(lSaveResultGraphics, ret.toString(), 0, 0);
+			
+			try {
+				ImageIO.write(lSaveResultImage, "PNG", new File(aPicturePath + "_toASCHII.png"));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			break;
+		case None:
+			break;
+		default:
+			break;
 		
-		// display result
-		BufferedImage lResultImage = new BufferedImage((int) (3000), (int)( 3000), BufferedImage.TYPE_INT_RGB);
-		Graphics lResultGraphics = lResultImage.createGraphics();
+		}
 		
-		lResultGraphics.setColor(Color.white);
-		lResultGraphics.fillRect(0, 0, lResultImage.getWidth(), lResultImage.getHeight());
-		
-		lResultGraphics.setFont( mFont );
-		lResultGraphics.setColor(Color.black);
-		drawString(lResultGraphics, ret.toString(), 0, 0);
-		
-		//lResultGraphics.drawString(ret.toString(), lResultImage.getWidth() / 2, lResultImage.getHeight() / 2);
-        JLabel label = new JLabel(new ImageIcon(lResultImage));  
-        JOptionPane.showMessageDialog(null, label);  
         
         
         
--- AschiiArtGenerator/src/Main.java
+++ AschiiArtGenerator/src/Main.java
@@ -11,8 +11,8 @@ public class Main {
 		} catch (Exception e) {
 		     e.printStackTrace();
 		}
-		String lInputFile = "C:\\tmp\\Wonder-Woman-AP (1).jpg";
-		int lColumns = 700;
+		String lInputFile = "C:\\tmp\\hilary-duff-high-resolution-wallpaper-1920x1200-0059.jpg";
+		int lColumns = 1500;
 //		
 //		System.out.println("sqrt");
 //		AschiiGenerator as = new AschiiGenerator( PixelAnalyzer.PixelScore.SquareRootMethod );
@@ -23,8 +23,8 @@ public class Main {
 //		System.out.println( as.generateTextFromPicture( lInputFile, lColumns ) );
 
 		System.out.println("lightness");
-		AschiiGenerator as = new AschiiGenerator( PixelAnalyzer.PixelScore.LightnessMethod, new Font("consolas", Font.BOLD, 3));
-		System.out.println( as.generateTextFromPicture(lInputFile, lColumns ) );
+		AschiiGenerator as = new AschiiGenerator( PixelAnalyzer.PixelScore.LightnessMethod, new Font("consolas", Font.PLAIN, 12));
+		System.out.println( as.generateTextFromPicture(lInputFile, lColumns, AschiiGenerator.PictureDestination.File ) );
 		
 //
 //		System.out.println("luminosity");
